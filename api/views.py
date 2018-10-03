610f7628e91014de2886ecb529d57cb7e88a0d72
jfsubrini <jfsubrini@zoho.com>
2018-10-04 00:46:13 +0200

    moving slowly to DRF

--- api/views.py
+++ api/views.py
@@ -1,81 +1,19 @@
-'''
-Module for views.
-'''
-
-import time
-
-from django.http import HttpResponse
-from django.views.decorators.http import require_GET, require_POST
-# from django.contrib.admin.views.decorators import staff_member_required
-from food.models import Food, Category
-from api.common import json_to_model
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 
-
-def performance(func):
-    '''Decorator creation to evaluate the performance of the API code'''
-    def inner(*args, **kwargs):
-        time_before = time.time()
-        returned_value = func(*args, **kwargs)
-        time_after = time.time()
-        running_time = time_after - time_before
-        print("La requête {0} a mis {1} pour s'exécuter".format(func, running_time))
-        return returned_value
-    return inner
-
-
-@require_GET
-# @staff_member_required
-@performance
-def product(request, product_id):
+class UserViewSet(viewsets.ModelViewSet):
     """
-    Method: GET
-    Path: /api/product/<id>
-    Get given product:
-    - id: the product ID
-    Return: the product as Json (model_to_json)
+    API endpoint that allows users to be viewed or edited.
     """
-    return Food.objects.get(id=product_id)
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
 
-# A TESTER CETTE VUE
-@require_POST
-# @staff_member_required
-@performance
-def product_create(request):
-    """
-    Method: POST
-    Path: /api/product/create
-    Create a product:
-    - data: customer fields as json in request body
-    Return: nothing
-    """
-    json_to_model(request.body.decode('utf-8'), Food).save()
-    return HttpResponse()
 
-@require_GET
-# @staff_member_required
-@performance
-def product_search(request):
+class GroupViewSet(viewsets.ModelViewSet):
     """
-    Method: GET
-    Path: /api/product/search
-    Search for products. Search parameters are passed as URL parameters. Thus to get products
-    with named Nutella, you would add parameters '?name=Nutella&brand=Ferrero'.
-    Return: the products list (queryset_to_json)
+    API endpoint that allows groups to be viewed or edited.
     """
-    filters = dict(request.GET.items())
-    return Food.objects.filter(**filters)
-
-@require_GET
-# @staff_member_required
-@performance
-def category_search(request):
-    """
-    Method: GET
-    Path: /api/category/search
-    Search for categories. Search parameters are passed as URL parameters.
-    Possible search by id : '?id=4'.
-    Return: the categories list (queryset_to_json)
-    """
-    filters = dict(request.GET.items())
-    return Category.objects.filter(**filters)
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
--- pur_beurre/urls.py
+++ pur_beurre/urls.py
@@ -18,11 +18,23 @@ Including another URLconf
 from django.urls import path, include
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework import routers
+from .. api import views
 
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('food.urls')),
-    path('api/', include('api.urls')),
+]
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns += [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
